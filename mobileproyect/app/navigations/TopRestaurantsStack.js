910ded19e4e77f21b5c4ed2ade63d790965d49a6
IvanST123 <iinf18.isanchezt@itesco.edu.mx>
2022-03-25 16:50:30 -0600

    clase 23 aplicado a ejemplo de restaurante

---
+++ mobileproyect/app/navigations/TopRestaurantsStack.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import TopRestaurants from '../screens/TopRestaurants'
+
+const Stack = createStackNavigator() 
+
+export default function TopRestaurantsStack() {
+    return(
+        <Stack.Navigator>
+                    
+                    <Stack.Screen
+                    
+                    name='toprestaurants'
+                    component={TopRestaurants}
+                    options={{title:'TopRestaurantes'}}
+                    
+                    />
+
+ 
+
+                    </Stack.Navigator>
+
+    )
+    
+}
